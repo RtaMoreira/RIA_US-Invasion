44ea2823604b850477e0baf07ad2fad9a45f942b
palavina95 <vivian.bridy@students.hevs.ch>
2019-05-12 17:08:44 +0200

    Add localisation to palmares

--- _javascript/game.js
+++ _javascript/game.js
@@ -35,6 +35,57 @@ document.body.appendChild(canvas);
 /********************************************************
 BEFORE THE GAME
 ********************************************************/
+/*Geolocation part*/
+var geoUser = "";
+
+if (localStorage.getItem("geoUserMemo") === null) { //if palmares is null we create it
+        localStorage.setItem("geoUserMemo", JSON.stringify(geoUser));
+}
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+   console.log("goeloc not supported");
+  }
+}
+
+
+function showPosition(position) {
+
+        var getJSON = function(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = function() {
+            var status = xhr.status;
+            if(status === 200){
+                callback(null, xhr.response);
+            } else {
+                callback(status, xhr.response);
+            }
+        };
+        xhr.send();
+        };
+
+        getJSON('http://open.mapquestapi.com/geocoding/v1/reverse?key=A5AOlcOT3M0rfeqBbrwMLBfHMZWDF1vZ&location='+position.coords.latitude+','+position.coords.longitude+'&includeRoadMetadata=true&includeNearestIntersection=true', function(err, data) {
+            if(err !== null){
+                alert('Something went wrong: ' + err);
+            } else {
+                var thisGeoUser = data["results"]["0"]["locations"]["0"]["adminArea5"]+"-"+data["results"]["0"]["locations"]["0"]["adminArea1"];
+                localStorage.setItem("geoUserMemo", JSON.stringify(thisGeoUser));
+            }
+        });
+}
+
+getLocation();
+
+geoUser = localStorage.getItem("geoUserMemo");
+geoUser = geoUser.substring(1,geoUser.length); //Delete first character
+geoUser = geoUser.slice(0,-1); //Delete last character
+
+/*End geolocations part*/
+
 var launchGame = function () {
     player1 = document.getElementById("player1").value;
     player2 = document.getElementById("player2").value;
@@ -656,7 +707,7 @@ function updateScore(theWinner, thePoints) {
 
     console.log("myValue :" + myValue);
 
-    currentPalmares.splice(myValue, 0, theWinner + '|' + thePoints);
+    currentPalmares.splice(myValue, 0, theWinner+','+geoUser+ '|' + thePoints);
 
     localStorage.setItem("palmares", JSON.stringify(currentPalmares));
 
